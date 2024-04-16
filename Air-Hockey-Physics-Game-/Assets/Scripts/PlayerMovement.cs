850f95a3d7aa20578584312d398f0589f5df45f8
KaydonBaggott <163126008+KaydonBaggott@users.noreply.github.com>
2024-04-16 03:29:56 +0200

    Restricted the movement of the player to be within the boundary

--- Air-Hockey-Physics-Game-/Assets/Scripts/PlayerMovement.cs
+++ Air-Hockey-Physics-Game-/Assets/Scripts/PlayerMovement.cs
@@ -11,11 +11,31 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D rb;
 
+    public Transform BoundaryHolder;
+
+    Boundary playerBoundary;
+
+    struct Boundary
+    {
+        public float Up, Down, Left, Right;
+
+        public Boundary(float up, float down, float left, float right)
+        {
+            Up = up; Down = down; Left = left; Right = right;
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
         playerSize = GetComponent<SpriteRenderer>().bounds.extents;
         rb = GetComponent<Rigidbody2D>();
+
+        playerBoundary = new Boundary(BoundaryHolder.GetChild(0).position.y,
+                                      BoundaryHolder.GetChild(1).position.y,
+                                      BoundaryHolder.GetChild(2).position.x,
+                                      BoundaryHolder.GetChild(3).position.x);
+
     }
 
     // Update is called once per frame
@@ -44,7 +64,11 @@ public class PlayerMovement : MonoBehaviour
 
             if (canMove)
             {
-                rb.MovePosition(mousePos);
+                Vector2 clampedMousePos = new Vector2(Mathf.Clamp(mousePos.x, playerBoundary.Left,
+                                                                  playerBoundary.Right),
+                                                      Mathf.Clamp(mousePos.y, playerBoundary.Down,
+                                                                  playerBoundary.Up));
+                rb.MovePosition(clampedMousePos);
             }
         }
         else
